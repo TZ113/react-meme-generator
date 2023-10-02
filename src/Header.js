d18de9ba0c5fabf566f3b17ad0ac87c0cc1e6fb6
TZ113 <earnestlearneran@gmail.com>
2023-10-02 21:58:58 +0600

    first commit

---
+++ src/Header.js
@@ -0,0 +1,11 @@
+
+export default function Header() {
+    return (
+        <header className="header">
+            <img className="header--image"
+                src="troll-face.png" alt="troll-face"
+            />
+            <h2 className="header--title">Meme Generator</h2>
+        </header>
+    )
+}
---
+++ src/Meme.js
@@ -0,0 +1,77 @@
+import { useEffect, useState } from "react"
+
+export default function Meme() {
+    const [meme, setMeme] = useState({
+        topText: "",
+        bottomText: "",
+        randomImage: "http://i.imgflip.com/1bij.jpg" 
+    })
+    const [allMemes, setAllMemes] = useState([])
+    
+/**
+useEffect takes a function as its parameter. If that function
+returns something, it needs to be a cleanup function. Otherwise,
+it should return nothing. If we make it an async function, it
+automatically returns a promise instead of a function or nothing.
+Therefore, if you want to use async operations inside of useEffect,
+you need to define the function separately inside of the callback
+function, as seen below:
+*/
+    useEffect(() => {
+        fetch("https://api.imgflip.com/get_memes")
+            .then(res => res.json())
+            .then(output => setAllMemes(output.data.memes))
+    }, [])
+    
+    function getMemeImage() {
+        const randomNumber = Math.floor(Math.random() * allMemes.length)
+        const url = allMemes[randomNumber].url
+        setMeme(prevMeme => {
+           return {...prevMeme,
+            randomImage: url
+}        })
+        
+    }
+    
+    function handleChange(event) {
+        const {name, value} = event.target
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            [name]: value
+        }))
+    }
+    
+    return (
+        <main>
+            <div className="form">
+                <input 
+                    type="text"
+                    placeholder="Top text"
+                    className="form--input"
+                    name="topText"
+                    value={meme.topText}
+                    onChange={handleChange}
+                />
+                <input 
+                    type="text"
+                    placeholder="Bottom text"
+                    className="form--input"
+                    name="bottomText"
+                    value={meme.bottomText}
+                    onChange={handleChange}
+                />
+                <button 
+                    className="form--button"
+                    onClick={getMemeImage}
+                >
+                    Get a new meme image 🖼
+                </button>
+            </div>
+            <div className="meme">
+                <img src={meme.randomImage} alt="meme_image" className="meme--image" />
+                <h2 className="meme--text top">{meme.topText}</h2>
+                <h2 className="meme--text bottom">{meme.bottomText}</h2>
+            </div>
+        </main>
+    )
+}
