37dd9f5338c15c877d812257ba618bdba991693f
TZ113 <earnestlearneran@gmail.com>
2023-11-30 20:22:33 +0600

    minor cupdates

--- src/Header.js
+++ src/Header.js
@@ -3,7 +3,7 @@ export default function Header() {
     return (
         <header className="header">
             <img className="header--image"
-                src="troll-face.png" alt="troll-face"
+                src="images/troll-face.png" alt="troll-face"
             />
             <h2 className="header--title">Meme Generator</h2>
         </header>
